0a8f62f924e98b47f0dc5ecad9d37e00fcb555ac
Orel <aurelien.dureux@utt.fr>
2022-10-05 10:48:19 +0200

    Approved assignment

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -6,45 +6,133 @@ import pandas as pd
 
 class KMeans:
     
-    def __init__(self):
+    def __init__(self,
+                 K=2,
+                 max_iterations=1000,
+                 init_type="random",
+                 tolerance=0.0001,
+                 normalize=False,
+                 multiple_init=False,
+                 ):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
-        pass
-        
+        self.max_iterations = max_iterations
+
+        self.X = []
+        self.K = K
+        self.clusters = [[] for _ in range(self.K)]
+        self.centroids = []
+        self.n_samples, self.n_features = (0, 0)
+
+        self.sse = None
+        self.tolerance = tolerance
+        self.init_type = init_type
+        self.normalize = normalize
+        self.multiple_init = multiple_init
+
     def fit(self, X):
         """
         Estimates parameters for the classifier
-        
+
         Args:
             X (array<m,n>): a matrix of floats with
                 m rows (#samples) and n columns (#features)
         """
-        # TODO: Implement
-        raise NotImplementedError()
-    
+
+        # Normalizing dataframe
+        if self.normalize:
+            for column in X.columns:
+                X[column] = (X[column] - X[column].min()) / (X[column].max() - X[column].min())
+
+        # Converto from a Dataframe to a Numpy array
+        self.X = X.to_numpy()
+        self.n_samples, self.n_features = np.shape(X)
+
+        # Initialize centroids with a method
+        # Doing multiple initialization to find the one with lowest SSE/Distortion
+        if self.multiple_init:
+            possible_centroids = {}
+            for _ in range(15):
+                self.centroids = self.initialize_centroids(X)
+                self.clusters = self.get_clusters()
+                possible_centroids[self.get_sse()] = self.centroids
+            self.centroids = possible_centroids[min(possible_centroids.keys())]
+        else:
+            self.centroids = self.initialize_centroids(X)
+
+        for i in range(self.max_iterations):
+            # Foreach point assign it to the closet cluster centroid
+            self.clusters = self.get_clusters()
+
+            # Compute the new centroid for each cluster
+            old_centroids = self.centroids
+            self.centroids = self.get_centroids()
+
+            # Check if centroids are the same
+            if np.all((old_centroids - self.centroids) == 0):
+                break
+
+            # Check if SSE is still having important variation
+            elif i > self.max_iterations/2:
+                old_sse = self.sse
+                self.sse = self.get_sse()
+                if old_sse is not None:
+                    if old_sse - self.sse < self.tolerance:
+                        break
+
     def predict(self, X):
         """
         Generates predictions
-        
+
         Note: should be called after .fit()
-        
+
         Args:
-            X (array<m,n>): a matrix of floats with 
+            X (array<m,n>): a matrix of floats with
                 m rows (#samples) and n columns (#features)
-            
+
         Returns:
             A length m integer array with cluster assignments
-            for each point. E.g., if X is a 10xn matrix and 
+            for each point. E.g., if X is a 10xn matrix and
             there are 3 clusters, then a possible assignment
             could be: array([2, 0, 0, 1, 2, 1, 1, 0, 2, 2])
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
+
+        # Foreach point assign it ot the closet cluster centroid
+        self.clusters = self.get_clusters()
+
+        # Putting in the correct form in 1D-array and clusters assignment [0, self.K-1]
+        z = np.zeros(self.n_samples, int)
+        for idx in range(self.n_samples):
+            for idx_cluster, cluster in enumerate(self.clusters):
+                if idx in cluster:
+                    z[idx] = idx_cluster
+                    break
+        return z
+
+    def initialize_centroids(self, X):
+        """
+        Args:
+            X: data as Dataframe
+
+        Returns:
+            (array<self.K, self.n_features>): a matrix of floats with  self.K rows (#first centroids)
+            and n columns (#features)
+        """
+        if self.init_type == "random":
+            return np.asarray(X.sample(n=self.K))
+        elif self.init_type == "kmeans++":
+            # Setting the first centroid randomly
+            centroids = [self.X[np.random.randint(0, self.n_samples)]]
+            # Selecting next centroids as the maximal distance between all points and their closest centroid
+            for idx in range(1, self.K):
+                distances = np.asarray([[min([np.linalg.norm(point-centroid) for centroid in centroids])] for point in self.X])
+                centroids.append(self.X[np.argmax(distances)])
+            return centroids
+
     def get_centroids(self):
         """
         Returns the centroids found by the K-mean algorithm
-        
+
         Example with m centroids in an n-dimensional space:
         >>> model.get_centroids()
         numpy.array([
@@ -56,13 +144,39 @@ class KMeans:
             [xm_1, xm_2, ..., xm_n]
         ])
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
-    
-    
-    
-# --- Some utility functions 
+        centroids = np.zeros((self.K, self.n_features))
+        for idx, idx_cluster in enumerate(self.clusters):
+            cluster = [self.X[i] for i in idx_cluster]
+            cluster_mean = np.mean(cluster, axis=0)
+            centroids[idx] = cluster_mean
+        return centroids
+
+    def get_clusters(self):
+        """
+        Returns:
+            (array<m,n>): a matrix of integers with self.K rows (#clusters)
+             and n columns (#points assigned in the clusters)
+        """
+        clusters = [[] for _ in range(self.K)]
+        for idx, point in enumerate(self.X):
+            closet_centroid = self.get_closet_centroid(point)
+            clusters[closet_centroid].append(idx)
+        return clusters
+
+    def get_closet_centroid(self, point):
+        dist_euclidean = np.zeros(self.K)
+        for idx, centroid in enumerate(self.centroids):
+            dist_euclidean[idx] = np.linalg.norm(centroid-point)
+        return np.argmin(dist_euclidean)
+
+    def get_sse(self):
+        self.sse = [0 for _ in range(self.K)]
+        for idx, cluster in enumerate(self.clusters):
+            for idx_point in cluster:
+                self.sse[idx] += (np.linalg.norm(self.X[idx_point] - self.centroids[idx]) ** 2).sum()
+        return np.sum(self.sse)
+
+# --- Some utility functions
 
 
 def euclidean_distortion(X, z):
@@ -89,6 +203,7 @@ def euclidean_distortion(X, z):
         
     return distortion
 
+
 def euclidean_distance(x, y):
     """
     Computes euclidean distance between two sets of points 
@@ -104,6 +219,7 @@ def euclidean_distance(x, y):
         of each x and y point
     """
     return np.linalg.norm(x - y, ord=2, axis=-1)
+
 
 def cross_euclidean_distance(x, y=None):
     """
@@ -158,8 +274,64 @@ def euclidean_silhouette(X, z):
     
     # Intra distance 
     a = D[np.arange(len(X)), z]
-    # Smallest inter distance 
+    # Smallest inter distance
     inf_mask = np.where(z[:, None] == clusters[None], np.inf, 0)
     b = (D + inf_mask).min(axis=1)
-    
+
     return np.mean((b - a) / np.maximum(a, b))
+
+
+def get_succes_rate(testing_iterations=100, verbose=True):
+    """
+    To get the success rate of the second data set by checking if the centroids are obtained in the right area.
+    Get the average Silhouette and Distortion on the correct prediction.
+    Args:
+        verbose: boolean allow the print or not about the progress
+        testing_iterations: integer that defines the number of execution of the k-means
+    """
+    data = pd.read_csv('data_2.csv')
+    data_X = data[['x0', 'x1']]
+
+    correct_predict = 0
+    distortion = []
+    silhouette = []
+    for i in range(testing_iterations):
+        if verbose:
+            print(f"Iteration n°{i}/{testing_iterations} - Correct prediction {correct_predict}")
+        correct_centroids = [[0.13279129, 0.84815637],
+                             [0.58220928, 0.18037957],
+                             [0.91298819, 0.75292059],
+                             [0.23372082, 0.11692144],
+                             [0.6884244, 0.59809779],
+                             [0.1741503, 0.65839862],
+                             [0.52354169, 0.87667458],
+                             [0.85885356, 0.53989269],
+                             [0.43386821, 0.28739245],
+                             [0.32555331, 0.65838786]]
+
+        # Running the model
+        k_means = KMeans(K=10, max_iterations=1000, init_type="kmeans++", normalize=True)
+        k_means.fit(data_X)
+        z = k_means.predict(data_X)
+
+        # Checking the correct centroids
+        threshold = 0.05
+        for centroid in k_means.centroids:
+            for idx, correct_centroid in enumerate(correct_centroids):
+                if np.linalg.norm(correct_centroid - centroid) < threshold:
+                    del correct_centroids[idx]
+                    break
+
+        # If the model is correct
+        if not correct_centroids:
+            correct_predict += 1
+            distortion.append(euclidean_distortion(data_X, z))
+            silhouette.append(euclidean_silhouette(data_X, z))
+
+    print(f"Succes rate : {correct_predict*100/testing_iterations}%")
+    print(f"Average Distortion {sum(distortion)/correct_predict:.3f}")
+    print(f"Average Silhouette {sum(silhouette)/correct_predict:.3f}")
+
+
+if __name__ == '__main__':
+    get_succes_rate(testing_iterations=1000)
--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -1,4 +1,7 @@
-import numpy as np 
+import math
+
+import numpy as np
+import numpy.ma
 import pandas as pd 
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
@@ -6,11 +9,17 @@ import pandas as pd
 
 class LogisticRegression:
     
-    def __init__(self):
-        # NOTE: Feel free add any hyperparameters 
-        # (with defaults) as you see fit
-        pass
-        
+    def __init__(self, max_iterations=1000, learning_rate=0.1, enrichment=False):
+        self.max_iterations = max_iterations
+
+        self.X = None
+        self.n_samples = None
+        self.n_features = None
+
+        self.weights = None
+        self.learning_rate = learning_rate
+        self.enrichment = enrichment
+
     def fit(self, X, y):
         """
         Estimates parameters for the classifier
@@ -21,9 +30,30 @@ class LogisticRegression:
             y (array<m>): a vector of floats containing 
                 m binary 0.0/1.0 labels
         """
-        # TODO: Implement
-        raise NotImplemented()
-    
+        # Set the intercept term 1 (not x0 here)
+        self.intercept_term(X)
+
+        # Add features the initial features
+        if self.enrichment:
+            self.features_enrichment()
+
+        # Retrieve specification data set
+        self.n_samples, self.n_features = self.X.shape
+
+        # Initialization of the weights to 0
+        self.weights = np.zeros(self.n_features)
+
+        for it in range(self.max_iterations):
+            # Stochastic gradient descent (each sample at the time | incremental)
+            for i in range(self.n_samples):
+                sample = self.X[i, :]
+                # Apply logistic function (sigmoid) on linear model
+                y_predicted = sigmoid(np.dot(sample, self.weights))
+                # Update proportional to the error term * current sample
+                dw = (y[i]-y_predicted)*sample
+                # Update weights
+                self.weights += self.learning_rate * dw
+
     def predict(self, X):
         """
         Generates predictions
@@ -38,12 +68,39 @@ class LogisticRegression:
             A length m array of floats in the range [0, 1]
             with probability-like predictions
         """
-        # TODO: Implement
-        raise NotImplemented()
-        
+        # Set the intercept term x0=1
+        self.intercept_term(X)
 
-        
-# --- Some utility functions 
+        # Add features the initial features
+        if self.enrichment:
+            self.features_enrichment()
+
+        # Putting all the prediction in one array
+        y_predicted = [sigmoid(np.dot(self.X[i, :], self.weights)) for i in range(self.X.shape[0])]
+
+        # Classify in positive/negative class
+        return np.asarray(y_predicted) > 0.5
+
+    def intercept_term(self, X):
+        """
+        Add the intercept term x0 = 1 to the data (not name x0 here)
+        Args:
+            X: data as a dataframe
+
+        """
+        x0 = np.ones((X.shape[0], 1))
+        self.X = np.append(np.asarray(X), x0, axis=1)
+
+    def features_enrichment(self):
+        """
+        Facilite the adding of features
+        - Currently only adding the absolute value of (x0 + x1)
+        """
+        x = np.asarray([[numpy.absolute(n[0]+n[1])] for n in self.X])
+        self.X = np.append(self.X, x, axis=1)
+
+# --- Some utility functions
+
 
 def binary_accuracy(y_true, y_pred, threshold=0.5):
     """
@@ -96,5 +153,3 @@ def sigmoid(x):
         Element-wise sigmoid activations of the input 
     """
     return 1. / (1. + np.exp(-x))
-
-        
